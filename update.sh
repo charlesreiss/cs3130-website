fec9eb681499c9d088c0f8ae43bdd1e5dc6aa6d9
cr4bd <cr4bd@virginia.edu>
2022-11-26 14:58:29 -0500

    do jekyll stuff in a separate directory

--- update.sh
+++ update.sh
@@ -3,10 +3,27 @@ umask 022
 #python3 quizzes_to_data.py    # XXX
 #python3 recordings_to_data.py # XXX
 chmod -R a+r files
-#chmod -R a+r slides
+chmod -R a+r slides
 pd=/u/cr4bd/bin/pandoc
+
+if [ ! -e jekyll-dir ]; then
+    mkdir -p jekyll-dir
+    pushd jekyll-dir
+    ln -s ../Gemfile .
+    ~/bin/bundle config set --local path vendor/bundle
+    ~/bin/bundle install
+    popd
+fi
+
+for input in assets Gemfile *.md _* files slides; do
+    if [ ! -e jekyll-dir/$input ]; then
+        ln -s ../$input jekyll-dir
+    fi
+done
+
 for input in *.pmd */*.pmd; do
-    output="${input%.pmd}.html"
+    output="jekyll-dir/${input%.pmd}.html"
+    mkdir -p $(dirname $output)
 #    if [ "$input" -nt "$output" ]; then 
         python3 env.py "$input" | \
         $pd --standalone \
@@ -20,4 +37,5 @@ for input in *.pmd */*.pmd; do
         chmod a+r $output
 #    fi
 done
+pushd jekyll-dir
 ~/bin/bundle exec jekyll b --trace
