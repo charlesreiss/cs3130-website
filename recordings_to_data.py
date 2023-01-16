f8c3f2f4da636a730c179e9808e286facdb929fe
cr4bd <cr4bd@virginia.edu>
2023-01-16 13:51:39 -0500

    recordings_to_data script; .gitignore update

---
+++ recordings_to_data.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import datetime
+import os
+import os.path
+import yaml
+import re
+
+
+def read_all_valid(dir):
+    result = []
+    for file in os.listdir(dir):
+        if not file.endswith('-valid.empty'):
+            continue
+        result.append({
+            'prefix': file.replace('-valid.empty', ''),
+        })
+    return result
+
+if __name__ == '__main__':
+    with open('./_data/recordings.yaml', 'w') as fh:
+        yaml.dump(read_all_valid('/u/cr4bd/public_html/3130/S2023/recordings'), stream=fh)
