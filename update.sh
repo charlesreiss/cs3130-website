03297266515961dc3da85a0b4443b95b5b7638f3
cr4bd <cr4bd@virginia.edu>
2023-01-16 13:50:43 -0500

    one quiz dropped; quizzes/recordings_to_data fixup

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #eval "$(~/bin/rbenv init -)"
 umask 022
-#python3 quizzes_to_data.py    # XXX
-#python3 recordings_to_data.py # XXX
+python3 quizzes_to_data.py    # XXX
+python3 recordings_to_data.py # XXX
 chmod -R a+r files
 chmod -R a+r readings/files
 chmod -R a+r slides
