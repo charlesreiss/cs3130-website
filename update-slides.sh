352f3ccc2a31f151e4031a4d97c430bce72414d6
cr4bd <cr4bd@virginia.edu>
2024-08-24 16:34:49 -0400

    CR slide importing in update-slides.sh

--- update-slides.sh
+++ update-slides.sh
@@ -3,3 +3,13 @@ mkdir -p slides
 pushd ../cs3130-slides; make
 cp _dist/*.pdf ../cs3130-website/slides
 popd
+pushd ../cs3130-slides/f2024-cr
+for i in 2024*; do
+    pushd $i
+    if [ -e final ] ; then
+        make copy-public-final
+    else
+        make copy-public-heldback
+    fi
+    popd
+done
