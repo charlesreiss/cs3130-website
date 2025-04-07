47ef03193f5cbae7e8f119573b62f993bf0b234c
cr4bd <cr4bd@virginia.edu>
2025-04-06 23:12:33 -0400

    rmv out-of-sync extracted version of life-c-skeleton

--- files/life/life-parallel.c
+++
@@ -1,6 +0,0 @@
-#include "life.h"
-#include <pthread.h>
-
-void simulate_life_parallel(int threads, LifeBoard *state, int steps) {
-    /* YOUR CODE HERE */
-}
--- files/life/timing.h
+++
@@ -1,5 +0,0 @@
-#include <stdint.h>
-
-typedef uint64_t cycles_type;
-
-extern cycles_type measure_function(void (*function)(), void (*setup)(), void (*teardown)());
